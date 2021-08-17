02e8aafdd75e419117c73a1e68c597d18c27b0fc
mokoshin0720 <shinya.work0720@gmail.com>
2021-08-17 14:35:32 +0900

    [add]usecases

---
+++ usecase/user_interactor.go
@@ -0,0 +1,23 @@
+// UseCase（interface/databaseへのInput Port, interface/controllersへのGateway）
+package usecase
+
+import "clean-architecture-tutorial/domain"
+
+type UserInteractor struct {
+	UserRepository UserRepository
+}
+
+func (interactor *UserInteractor) Add(u domain.User) (err error) {
+	_, err = interactor.UserRepository.Store(u)
+	return
+}
+
+func (interactor *UserInteractor) Users() (user domain.Users, err error) {
+	user, err = interactor.UserRepository.FindAll()
+	return
+}
+
+func (interactor *UserInteractor) UserByID(identifier int) (user domain.User, err error) {
+	user, err = interactor.UserRepository.FindById(identifier)
+	return
+}
---
+++ usecase/user_repository.go
@@ -0,0 +1,9 @@
+package usecase
+
+import "clean-architecture-tutorial/domain"
+
+type UserRepository interface {
+	Store(domain.User) (int, error)
+	FindById(int) (domain.User, error)
+	FindAll() (domain.Users, error)
+}
